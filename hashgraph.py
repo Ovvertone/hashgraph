a6b659f7c6c90cde4053ee29a889ddcd4fe25160
Vyacheslav <shirkeev@example.com>
2020-11-21 20:27:11 +0700

    Refactor create_node and graph_consensus

--- hashgraph.py
+++ hashgraph.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, random
 from time import time
 
 import hashlib
@@ -8,36 +8,53 @@ class Hashgraph:
     NODES = []
 
     def __init__(self):
-        self.NODES.append({
-            'timestamp': self.encrypt(str(time())),
-            'transaction': None,
-            'previous_hash': None,
-            'awareness_nodes': [],
-            'id': 'genesis',
-        })
+        pass
 
     def create_node(self, data: dict):
         node = {
+            'id': self.encrypt(str(random() * time())),
             'timestamp': self.encrypt(str(time())),
             'transaction': self.encrypt(data['login']) + self.encrypt(data['password']),
-            'previous_hash': self.NODES[-1]['id'],
+            # 'previous_hash': self.NODES[-1]['id'] if self.NODES > 0 else None,
             'awareness_nodes': [],
         }
-        node['id'] = self.encrypt(str(node))
-        if len(self.NODES) > 2:
-            [node['awareness_nodes'].append(awareness_node_id) for awareness_node_id in self.graph_consensus()]
+        # node['id'] = self.encrypt(str(node)) if node['previous_instance'] == None else node['id']
+        node['previous_instance'] = self.find_by_id(node['id'])
+        node['awareness_nodes'].append(self.find_by_id(node['id']))
+        try:
+            [node['awareness_nodes'].append(id_) for id_ in self.graph_consensus(node['id'])]
+        except TypeError as e:
+            print(e)
+            pass
         self.NODES.append(node)
 
     @staticmethod
     def encrypt(value: str) -> str:
         return hashlib.md5(value.encode()).hexdigest()
 
-    def graph_consensus(self):
-        awareness_node_1 = choice(self.NODES[1:])
-        awareness_node_2 = choice(self.NODES[1:])
-        while awareness_node_1 == awareness_node_2:
-            awareness_node_2 = choice(self.NODES[1:])
-        return [awareness_node_1['id'], awareness_node_2['id']]
+    def graph_consensus(self, parent_id):
+        if len(self.NODES) == 0 or len(self.get_unique_users()) < 2:
+            return None
+        parent_nodes = []
+        awareness_node = choice(self.NODES)
+        while awareness_node['id'] == parent_id:
+            awareness_node = choice(self.NODES)
+        parent_nodes.append(awareness_node['id'])
+        if len(awareness_node['awareness_nodes']) > 1:
+            for id_ in awareness_node['awareness_nodes'][1:]:
+                parent_nodes.append(id_)
+        return parent_nodes
+
+    def get_unique_users(self):
+        unique_users = []
+        [unique_users.append(node) for node in self.NODES if node not in unique_users]
+        return unique_users
+
+    def find_by_id(self, id):
+        for node in reversed(self.NODES):
+            if node['id'] == id:
+                return node
+        return None
 
 
 graph = Hashgraph()
@@ -47,6 +64,5 @@ graph.create_node({'login': 'Slava', 'password': 'PITON-GOVNO'})
 graph.create_node({'login': 'Qverty', 'password': '123456'})
 graph.create_node({'login': 'Asdfgh', 'password': '456789'})
 graph.create_node({'login': 'Zxcvbn', 'password': '789123'})
+
 [print(i) for i in graph.NODES]
-
-
